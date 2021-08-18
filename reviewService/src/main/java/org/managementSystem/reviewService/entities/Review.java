2888273ae0de6eb06dc2264c0bc21e77d3cf1b3b
Adheesh1997 <adheeshkulathunga@gmail.com>
2021-08-19 01:20:36 +0530

    List of reviews is included

--- reviewService/src/main/java/org/managementSystem/reviewService/entities/Review.java
+++ reviewService/src/main/java/org/managementSystem/reviewService/entities/Review.java
@@ -3,6 +3,7 @@ package org.managementSystem.reviewService.entities;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "review_table")
@@ -10,26 +11,40 @@ public class Review {
     @Id
     //@GeneratedValue(strategy = GenerationType.AUTO)
     String id;
-    String review;
+    //String review;
+    @ElementCollection
+    List<String> reviewList;
 
     public Review() {
     }
 
-    public Review(String id, String review) {
+    /*public Review(String id, String review) {
         this.id = id;
         this.review = review;
+    }*/
+
+    public Review(String id, List<String> reviewList) {
+        this.id = id;
+        this.reviewList = reviewList;
+    }
+
+    public List<String> getReviewList() {
+        return reviewList;
+    }
+
+    public void setReviewList(List<String> reviewList) {
+        this.reviewList = reviewList;
     }
 
     public String getId() {
         return id;
     }
 
-
-    public String getReview() {
+    /*public String getReview() {
         return review;
-    }
+    }*/
 
-    public void setReview(String review) {
+    /*public void setReview(String review) {
         this.review = review;
-    }
+    }*/
 }
